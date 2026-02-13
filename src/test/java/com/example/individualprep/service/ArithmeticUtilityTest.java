d1a82b530299ce5cb9cd40c595d48481682bdc7a
angelojohenry <johenry.angelo@gmail.com>
2026-02-13 18:57:50 +0700

    added tests for arithmetic addition

--- src/test/java/com/example/individualprep/service/ArithmeticUtilityTest.java
+++ src/test/java/com/example/individualprep/service/ArithmeticUtilityTest.java
@@ -27,4 +27,16 @@ class ArithmeticUtilityTest {
     void testMultiplyWithNegativeNumber() {
         assertEquals(-25.0, arithmeticUtility.multiply(5, -5));
     }
+
+    @Test
+    void testAddPositiveNumbers() { assertEquals(8.0, arithmeticUtility.add(5,3)); }
+
+    @Test
+    void testAddNegativeNumbers() { assertEquals(-8.0, arithmeticUtility.add(-5,-3));}
+
+    @Test
+    void testAddPositiveAndNegative() { assertEquals(2.0, arithmeticUtility.add(5,-3)); }
+
+    @Test
+    void testAddWithZero() { assertEquals(5.0, arithmeticUtility.add(5,0));}
 }
